711adb5c0082e13db9ded8ce28aaa3f01d7474e2
Kevin Manuel Espejel Martinez <kevin.espejel@coppel.com>
2023-06-06 18:01:40 -0600

    port modified

--- src/main.py
+++ src/main.py
@@ -69,4 +69,4 @@ app.openapi = custom_openapi
 container.SingletonContainer.init()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False)
